e6bde07bbfad4cab79eced549a03d554a91b107d
admin <andrewhoyer@gmail.com>
2023-02-19 17:22:20 -0800

    Removed 1 from deck

--- deck.py
+++ deck.py
@@ -15,7 +15,7 @@ class Deck:
 	
 	def __init__(self):
 		self.suits  = ['H', 'S', 'D', 'C']
-		self.labels = ['A', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+		self.labels = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
 		self.deck   = self.reset_deck(self.labels, self.suits)
 	
 	
