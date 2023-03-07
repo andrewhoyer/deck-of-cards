2f9bd277aa16f0fec014dc6a164c7198d7d7a58f
admin <andrewhoyer@gmail.com>
2023-03-06 20:19:39 -0800

    Deck and suit ranking, corrected ordering of suits and labels

--- deck.py
+++ deck.py
@@ -6,16 +6,22 @@ class Deck:
 	def reset_deck(self, labels, suits):
 		deck = []
 		
+		deck_position = 1
 		for suit in suits:
+			suit_position = 1
 			for label in labels:
-				deck.append(card.Card(label, suit))
+				deck.append(card.Card(label, suit, deck_position, suit_position))
+				suit_position = suit_position + 1
+				deck_position = deck_position + 1
   		
 		return deck
 	
 	
 	def __init__(self):
-		self.suits  = ['H', 'S', 'D', 'C']
-		self.labels = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+
+		# The order of suits and labels is specific for card ranking
+		self.suits  = ['C', 'D', 'H', 'S']
+		self.labels = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 		self.deck   = self.reset_deck(self.labels, self.suits)
 	
 	
@@ -32,5 +38,7 @@ class Deck:
 			return self.deck[position - 1]
 		else:
 			return False
-
+	
+	def deal_card(self):
+		return(self.deck.pop(0))
 
